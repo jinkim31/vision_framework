f3e110def164d64948fe4e367db680a306f01df1
jinkim31 <jinkim@kaist.ac.kr>
2023-04-11 19:48:39 +0900

    dockspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,41 @@
 #include "immapp/immapp.h"
 #include "implot/implot.h"
 #include "immvision/immvision.h"
-#include "imgui_md_wrapper.h"
 #include "eventThread.h"
 #include <opencv2/opencv.hpp>
 #include "custom_widgets.h"
 
+cv::Mat mat;
 void Gui()
 {
-    Widget::setStyle();
+    //Widget::setStyle();
     ImPlot::ShowDemoWindow();
     ImGui::ShowDemoWindow();
-    auto mat = cv::imread("../assets/world.jpg");
-    ImmVision::ImageDisplay("image display!", mat);
 
-    ImmVision::ImageParams p;
-    ImmVision::Image("image display!", mat, &p);
+    /*
+    ImGui::Begin("Image view");
+    {
+        std::cout << "avail:" << ImGui::GetContentRegionAvail().x << "," << ImGui::GetContentRegionAvail().y
+                  << std::endl;
+        ImmVision::ImageDisplay("", mat, {(int) ImGui::GetContentRegionAvail().x / 2,
+                                          (int) ImGui::GetContentRegionAvail().y / 2}, true);
+    }ImGui::End();
+     */
 }
 
 int main(int , char *[])
 {
-    HelloImGui::SimpleRunnerParams runnnerParams;
-    runnnerParams.guiFunction = Gui;
-    runnnerParams.windowSize = {600, 800};
+    mat = cv::imread("../assets/world.jpg");
+    HelloImGui::RunnerParams runnerParams;
+    runnerParams.callbacks.SetupImGuiStyle = Widget::setStyle;
+    runnerParams.callbacks.ShowGui = Gui;
+    runnerParams.imGuiWindowParams.defaultImGuiWindowType = HelloImGui::DefaultImGuiWindowType::ProvideFullScreenDockSpace;
+    runnerParams.imGuiWindowParams.enableViewports = false;
 
     ImmApp::AddOnsParams addOnsParams;
     addOnsParams.withMarkdown = true;
     addOnsParams.withImplot = true;
 
-    ImmApp::Run(runnnerParams, addOnsParams);
+    ImmApp::Run(runnerParams, addOnsParams);
     return 0;
 }
